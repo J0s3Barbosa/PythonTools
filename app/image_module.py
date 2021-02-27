51902f0c32789dacb01a476a347b18545b5b7722
J0s3Barbosa <appchto@gmail.com>
2021-02-27 00:09:07 -0300

    +

--- app/image_module.py
+++ app/image_module.py
@@ -4,9 +4,6 @@ from flask import jsonify, request, Blueprint
 from app.image import ImagesDAO
 import pymongo
 from flask.templating import render_template
-import numpy as np 
-import pyautogui 
-import time
 import os
 from app.config.app_config import mongo_config
 
@@ -38,11 +35,6 @@ class ImageModule():
     def GetScreenShot():
         # imagem = ImageGrab.grab()
         # imagem.save('screenShot1.jpg', 'jpeg')
-        
-        # take screenshot using pyautogui 
-        image = pyautogui.screenshot() 
-        image_dao.insert_image(image)
-        
         return 'GetScreenShot' 
 
     @imageModule.route('/CountScreenShot')
