1b3046346d157ef7c013be866909c85390584d5f
J0s3Barbosa <appchto@gmail.com>
2021-02-27 00:02:50 -0300

    1

--- app/image_module.py
+++ app/image_module.py
@@ -5,7 +5,6 @@ from app.image import ImagesDAO
 import pymongo
 from flask.templating import render_template
 import numpy as np 
-import cv2 
 import pyautogui 
 import time
 import os
@@ -42,19 +41,6 @@ class ImageModule():
         
         # take screenshot using pyautogui 
         image = pyautogui.screenshot() 
-        
-        # since the pyautogui takes as a  
-        # PIL(pillow) and in RGB we need to  
-        # convert it to numpy array and BGR  
-        # so we can write it to the disk 
-        image = cv2.cvtColor(np.array(image), 
-                            cv2.COLOR_RGB2BGR) 
-        file_name_timestr = time.strftime("%Y%m%d-%H%M%S")
-        print(image_folder+ file_name_timestr+'.jpg')
-
-        # writing it to the disk using opencv 
-        cv2.imwrite(os.path.join(image_folder+ file_name_timestr+'.jpg') , image) 
-
         image_dao.insert_image(image)
         
         return 'GetScreenShot' 
